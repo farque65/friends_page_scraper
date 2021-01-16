9613b7cc114d3e8f65058505efb5c6fde4d97098
farque65 <fahim.farque@gmail.com>
2021-01-16 17:47:47 -0500

    [FPS-001] Added docker compose

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # friends_page_scraper
 Script to scrape and insert data to database
+
+# Start up Command
+sudo docker-compose -f ./docker-compose.yml up -d
--- postgres/init.sql
+++ postgres/init.sql
@@ -1,6 +1,3 @@
-CREATE USER postgres;
-DROP DATABASE IF EXISTS db_friends;
-CREATE DATABASE db_friends;
 CREATE TABLE IF NOT EXISTS table_script_lines(
 	character CHAR(50),
 	line_number INT,
@@ -10,16 +7,17 @@ CREATE TABLE IF NOT EXISTS table_script_lines(
 CREATE TABLE IF NOT EXISTS table_episodes(
 	episode_number INT,
 	season INT,
-	description VARCHAR(255)
+	description VARCHAR(255),
+	link VARCHAR(255)
 );
 CREATE TABLE IF NOT EXISTS table_characters (
 	full_name VARCHAR(255),
 	first_name VARCHAR(255),
 	picture bytea
 );
-GRANT ALL PRIVILEGES ON DATABASE db_friends TO docker;
-INSERT INTO table_characters(full_name, first_name) VALUES (Rachel Green, Rachel);
-INSERT INTO table_characters(full_name, first_name) VALUES (Joey Tribbiani, Joey);
-INSERT INTO table_characters(full_name, first_name) VALUES (Phoebe Buffay, Phoebe);
-INSERT INTO table_characters(full_name, first_name) VALUES (Ross Geller, Ross);
-INSERT INTO table_characters(full_name, first_name) VALUES (Chandler Bing, Chandler);
+GRANT ALL PRIVILEGES ON DATABASE db_friends TO postgres;
+INSERT INTO table_characters(full_name, first_name) VALUES ('Rachel Green', 'Rachel');
+INSERT INTO table_characters(full_name, first_name) VALUES ('Joey Tribbiani', 'Joey');
+INSERT INTO table_characters(full_name, first_name) VALUES ('Phoebe Buffay', 'Phoebe');
+INSERT INTO table_characters(full_name, first_name) VALUES ('Ross Geller', 'Ross');
+INSERT INTO table_characters(full_name, first_name) VALUES ('Chandler Bing', 'Chandler');
